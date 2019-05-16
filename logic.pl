e769045386579a87b46c4d3656efc692d24b9ccf
Aleksandar Cvejic <cvejicaca@gmail.com>
2019-05-16 18:49:23 +0200

    Added Probabilities to prolog

--- logic.pl
+++ logic.pl
@@ -1,6 +1,8 @@
-/* Usage: number_of_symptoms(List_of_symptoms, Condition, Number).
- number_of_symptoms(List_of_symptoms, Condition, List_of_Syptoms_for_said_condition, Number_of_truths) */
-number_of_symptoms(L, Con, Num):- condition(Con, Rlist), number_of_symptoms(L, Con, Rlist, Num), Num > 0.
-number_of_symptoms([], Cond, List, 0).
-number_of_symptoms([S|R], Cond, List, Num):- member(S,List), number_of_symptoms(R, Cond, List, Number), Num is Number + 1. 
-number_of_symptoms([S|R], Cond, List, Num):- \+ member(S, List), number_of_symptoms(R, Cond, List, Num).
+/* Usage: number_of_symptoms(List_of_symptoms, Condition, Number, Probability).
+ number_of_symptoms(List_of_symptoms, Condition, List_of_Syptoms_for_said_condition, Number_of_truths, Probability_Sum) */
+
+number_of_symptoms(L, Con, Num, ProbOut):- condition(Con, Rlist), number_of_symptoms(L, Con, Rlist, Num, Prob), Num > 0, ProbOut is  Prob / Num.
+number_of_symptoms([], Cond, List, 0, 0).
+number_of_symptoms([S|R], Cond, List, Num, Prob):- member(S,List), number_of_symptoms(R, Cond, List, Number, PreviousProb), Num is Number + 1, symptom_relation(Cond, S, OutProb), Prob is OutProb + PreviousProb. 
+number_of_symptoms([S|R], Cond, List, Num, Prob):- \+ member(S, List), number_of_symptoms(R, Cond, List, Num, Prob). 
+
