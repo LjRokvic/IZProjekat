28b45de22ca3da65424776e0db62aced0e215315
LjRokvic <ljubomirrokvic@gmail.com>
2019-05-23 16:21:27 +0200

    Added preventive CBR

---
+++ CaseBased/src/model/PreventiveContainer.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreventiveContainer {
+	
+	private List<PreventiveDescription> cases = new ArrayList<PreventiveDescription>();
+
+	public List<PreventiveDescription> getCases() {
+		return cases;
+	}
+
+	public void setCases(List<PreventiveDescription> cases) {
+		this.cases = cases;
+	}
+
+	public PreventiveContainer() {
+		super();
+	}
+
+	
+}
